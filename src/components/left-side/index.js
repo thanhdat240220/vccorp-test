895fc88d477c8a0655d0ba0d310be4b0bf76601c
DatTran <thanhdat240220@gmail.com>
2024-11-05 10:17:15 +0700

    update

--- src/components/left-side/index.js
+++ src/components/left-side/index.js
@@ -28,20 +28,20 @@ const LeftSide = ({
 
   return (
     <div className="relative">
-      <div className="image-container max-h-fit -mt-[1.7rem] sm:mt-0">
+      <div className="image-container max-h-fit -mt-[1.85rem] sm:mt-0">
         <HexagonImageType1
           size={isMobile ? 'sm' : 'md'}
           src={imagesConfig.hexagonType1Url}
         />
       </div>
-      <div className="image-container max-h-fit absolute top-[55%] left-[30%]">
+      <div className={`image-container max-h-fit absolute top-[55%] ${isMobile ? 'left-[120px]' : 'sm:left-[170px]'}`}>
         <HexagonImageType2
           size={isMobile ? 'sm' : 'md'}
           src={imagesConfig.hexagonType2Url}
         />
       </div>
-      <div className="absolute top-[15%] right-[20%] sm:top-[5%] sm:right-[5%] z-20">
-        <Hexagon color="bg-shapeColors-light" size="w-14 h-8 sm:w-[9.1875rem] sm:h-[5.25rem]" opacity="opacity-30" radius='rounded' />
+      <div className="absolute top-[18%] right-[18%] sm:top-[8%] sm:right-[5%] z-20">
+        <Hexagon color="bg-shapeColors-light" size="w-14 h-8 sm:w-[9.1875rem] sm:h-[5.25rem]" opacity="opacity-30" radius='rounded-sm sm:rounded' />
       </div>
     </div>
   );
--- src/components/shapes/index.js
+++ src/components/shapes/index.js
@@ -1,3 +1,4 @@
+import './index.scss'
 import Hexagon from "./hexagon/Hexagon";
 import HexagonBottom from "./bottom-hexagon-svg/BottomHexagonSvg";
 import PlusIcon from "./plus/Plus";
@@ -5,34 +6,34 @@ import Square from "./square/Square";
 
 const Shapes = () => {
     return (
-        <div>
-            <div className="absolute bottom-0 left-[49%] z-10">
+        <div className='shapes'>
+            <div className="absolute bottom-0 left-[48%] z-10">
                 <PlusIcon type={1} />
             </div>
             <div className="absolute right-0 top-[7.5rem] sm:top-[1.375rem]">
                 <PlusIcon type={2} />
             </div>
 
-            <div className="absolute top-[28rem] -right-4 sm:top-72 sm:-right-12">
+            <div className="absolute top-[27rem] -right-4 sm:top-72 sm:-right-12">
                 <Hexagon color="bg-shapeColors-dark" size="w-8 h-[1.125rem] sm:w-[5.3125rem] sm:h-[3.125rem]" opacity="opacity-[26%]" />
             </div>
-            <div className="absolute top-[31.5rem] sm:top-[30.625rem] left-[75%]">
+            <div className="absolute top-[31.25rem] sm:top-[30.625rem] left-[75%]">
                 <Hexagon color="bg-shapeColors-dark" size="w-6 h-4 sm:w-[4rem] sm:h-[2.25rem]" />
             </div>
 
-            <div className="absolute bottom-[5rem] left-[81%] sm:top-[45%] sm:left-[52%]">
+            <div className="absolute top-[34rem] left-[81%] sm:top-[45%] sm:left-[50%]">
                 <Square color="bg-white" size="w-6 h-6 sm:w-[4.75rem] sm:h-[4.75rem]" opacity="opacity-30" radius="rounded-md sm:rounded-2xl"/>
             </div>
-            <div className="absolute bottom-[9.625rem] left-[52%] sm:bottom-[10.625rem] sm:left-[65%]">
-                <Square color="bg-white" size="w-7 h-7 sm:w-[2.8125rem] sm:h-[2.8125rem]" opacity="opacity-30" radius="rounded-md sm:rounded-2xl" />
+            <div className="absolute bottom-[10rem] left-[52%] sm:bottom-[9rem] sm:left-[63%]">
+                <Square color="bg-white" size="w-7 h-7 sm:w-[2.8125rem] sm:h-[2.8125rem]" opacity="opacity-30" radius="rounded-md sm:rounded-xl" />
             </div>
-            <div className="absolute bottom-[5.75rem] right-[25%] sm:bottom-[13.25rem] sm:right-[15%]">
+            <div className="absolute bottom-[5.75rem] right-[27%] sm:bottom-[13.25rem] sm:right-[15%]">
                 <Square color="bg-white" size="w-[0.625rem] h-[0.625rem] sm:w-16 sm:h-16" opacity="opacity-20" radius="rounded-sm sm:rounded-2xl" />
             </div>
             <div className="absolute bottom-[3rem] right-[6.875rem] sm:top-[13.125rem] sm:right-[10%]">
                 <Square color="bg-white" size="w-[1.125rem] h-[1.125rem] sm:w-16 sm:h-16" opacity="opacity-20" radius="rounded-sm sm:rounded-2xl" />
             </div>
-            <div className="absolute bottom-[14rem] right-[2.125rem] sm:bottom-[16rem] sm:right-[31%]">
+            <div className="absolute bottom-[14rem] right-[2.125rem] sm:bottom-[15.75rem] sm:right-[31%]">
                 <Square color="bg-white" size="w-6 h-6 sm:w-[1.625rem] sm:h-[1.625rem]" opacity="opacity-10 sm:opacity-20" radius="rounded-md" />
             </div>
 
